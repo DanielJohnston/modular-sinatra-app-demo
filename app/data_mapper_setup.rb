2a39818fdee6a0013991df113bff5ad88866bfe0
DanielJohnston <macademy@danieljohnston.co.uk>
2015-05-20 15:28:30 +0100

    more datamapper fixes

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,10 +2,8 @@
 #  checking what environment we're in, and defaulting to development:
 env = ENV['RACK_ENV'] || 'development'
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-if env == 'development'||'test'
-  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-else
-  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-end
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
+
 DataMapper.finalize # After declaring your models, you should finalise them
 # However, the database tables don't exist yet. Let's tell datamapper to create them
+DataMapper.auto_upgrade!

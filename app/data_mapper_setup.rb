b6eef351908eb53f02b9a212b73787cb1faae0f8
DanielJohnston <macademy@danieljohnston.co.uk>
2015-05-20 15:19:23 +0100

    tiny data mapper error

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@
 #  checking what environment we're in, and defaulting to development:
 env = ENV['RACK_ENV'] || 'development'
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-if env = 'development'||'test'
+if env == 'development'||'test'
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 else
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
